da530cfcbe054c2734f94239712591863575429e
Ayoubbbb13 <117095826+Ayoubbbb13@users.noreply.github.com>
2023-12-17 20:29:48 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "morpion.h"
 
-int main(int argc, char ** argv){
+//int main(int argc, char ** argv){
 	/*
 	grille test = {CROIX, CROIX, CROIX, ROND, ROND, ROND, ROND, CROIX, CROIX};
 	morpion test2;
@@ -11,7 +11,7 @@ int main(int argc, char ** argv){
 	printf("Le trait est au : %d \n", test2.trait);
 	*/
 	
-	morpion M = newMorpion();
+	/*morpion M = newMorpion();
 	printMorpion(M);
 	playMorpion(&M, 0);
 	playMorpion(&M, 1);
@@ -20,8 +20,26 @@ int main(int argc, char ** argv){
 	playMorpion(&M, 6);
 	printMorpion(M);
 	generateMorpionImage(M);
-	printf("Est-ce que quelqu'un a gagné ? %d \n", isWin(M));
+	printf("Est-ce que quelqu'un a gagné ? %d \n", isWin(M));*/
 	
-	return 0;
+	
+        int main(){
+        morpion M = newMorpion();
+        int move;
+        int currentPlayer = 0;  // 0 for player 1, 1 for player 2
 
+        while(!isWin(M)){  // Check if the game is over
+        printMorpion(M);  // Display the current state of the game
+        printf("Player %d's turn. Enter your move (0-8): ", currentPlayer + 1);
+        scanf("%d", &move);
+        playMorpion(&M, move);  // Update the game state with the new move
+        generateMorpionImage(M);  // Generate an image of the current game state
+        currentPlayer = 1 - currentPlayer;  // Switch player
+    }
+
+        printMorpion(M);  // Display the final state of the game
+        printf("Game over!\n");
+
+    return 0;
 }
+
