8461da9dcdc0cd304c34de9579a9705a7fb38f85
mrimbert <147839832+mrimbert@users.noreply.github.com>
2024-01-09 21:18:04 +0100

    Add files via upload

--- sm-bot/sm-bot.c
+++ sm-bot/sm-bot.c
@@ -7,29 +7,39 @@
 #include "negamax.h"
 
 int main(int argc, char* argv[]){
-	int secondsleft;
-	printf("Temps restant: ");
-	scanf("%d", &secondsleft);
-    	time_t start_time = time(NULL);
+
+	int secondsleft = atoi(argv[4]);
+	
+	char* FEN = argv[1];
+	//printf("Lu : %s \n", FEN);
+	char* lastMove = argv[2];
+	//printf("Lu : %s \n", lastMove);
+	char* trait = argv[3];
+	//printf("Lu : %s \n", trait);
     	
-	super_morpion sm1 = toSuperMorpion(str);
-	int i=0;
-	while (str[i] != ' '){
-		i++;
-	}
-	char a = str[i+2];
-	int grilleAJouer = a - '0';
+	super_morpion sm1 = toSuperMorpion(FEN, trait);
+	//printf("Trait : %d \n", sm1.trait);
+
 	childNode node;
 	node.sm  = sm1;
-	node.dernierePosition = grilleAJouer;
+	node.dernierePosition = atoi(lastMove);
 	showSuperMorpion(&sm1);
-
-	int profondeur = 5;
+	
+	int profondeur;
+	
+	
+	if(secondsleft >= 120) profondeur = 15;
+	if(secondsleft >= 60 && secondsleft < 120 ) profondeur = 10;
+	if(secondsleft <= 30) profondeur = 5;
+	if(secondsleft < 15) profondeur = 3;
+	
+	//printf("Profondeur choisie : %d \n", profondeur);
+	
 	childNode result;
 	result = getBestMove(&sm1,profondeur,1,node.dernierePosition);
 	showSuperMorpion(&result.sm);
 	int pos = convCoup(result.dernierePosition);
-	printf("%d",pos);
+	printf("Le coup joué est : %d \n",pos);
 	return 0;
 }
 
